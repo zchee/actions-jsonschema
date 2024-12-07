55b6c837bec8f6f54348f8e527fb614f41400058
Koichi Shiraishi <zchee.io@gmail.com>
2024-12-07 23:12:56 +0900

    main: add main.go stub
    Signed-off-by: Koichi Shiraishi <zchee.io@gmail.com>

---
+++ main.go
@@ -0,0 +1,10 @@
+// Copyright 2024 The actions-jsonschema Authors
+// SPDX-License-Identifier: Apache-2.0
+
+package main
+
+import (
+	_ "github.com/santhosh-tekuri/jsonschema/v6"
+)
+
+func main() {}
